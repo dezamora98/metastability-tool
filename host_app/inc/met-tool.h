e76c3578a51fe6ea6e59bd674bb00857e30898af
dezamora98 <dezamora98@gmail.com>
2024-02-01 00:18:09 -0500

    comenzando estructura, hay que ver el tema del ADC, estoy trabajando en una carptera aparte

--- host_app/inc/met-tool.h
+++ host_app/inc/met-tool.h
@@ -3,18 +3,33 @@
 
 #include <stdint.h>
 
-typedef struct{
-    uint32_t n_met;
-    float temp_chip;
-    float volt_source;
-}tool_frame;
+/**
+ * @struct mtool_exp_data
+ * @brief Structure to store the result of the experiment.
+ */
+typedef struct
+{
+    uint32_t n_met;     ///< Number of metastability events.
+    float temp_chip;    ///< Chip temperature
+    float power_chip;   ///< Chip power supply
+} mtool_exp_data;
 
-typedef struct{
-    uint32_t n_met;
-    float temp_chip;
-    float volt_source;
-}host_frame;
+
+/**
+ * @struct m_tool_param
+ * @brief Structure for defining the experiment setup parameters.
+ */
+typedef struct 
+{
+    uint64_t exp_clock;
+    uint64_t sample_rate;
+
+}mtool_exp_param;
 
 
+void mtool_set_param();
+void mtool_start();
+void mtool_stop();
+void mtool_get_data(mtool_exp_data *data);
 
 #endif // !MET_TOOL_H
